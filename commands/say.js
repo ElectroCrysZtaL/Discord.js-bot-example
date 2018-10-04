d5042aa509d226825cd6c09d9f7d88fb1e8ccd0c
ElectroCrysZtaL <41235770+ElectroCrysZtaL@users.noreply.github.com>
2018-10-04 17:40:55 +0700

    replaced "command" into exports.run

--- commands/say.js
+++ commands/say.js
@@ -1,4 +1,4 @@
-if(command === "say") {
+exports.run = (client, message, args) => {
     const sayMessage = args.join(" ");
     message.delete().catch(O_o=>{}); // Ignores the error 
     message.channel.send(sayMessage);
