d0eeca36e471ca59b0d146d57aad80655b8449f2
ElectroCrysZtaL <41235770+ElectroCrysZtaL@users.noreply.github.com>
2018-10-04 17:15:33 +0700

    added ping command

--- commands/ping.js
+++ commands/ping.js
@@ -1,1 +1,7 @@
 
+exports.run = (client, message, args) => {
+ if(command === "ping") {
+    const m = await message.channel.send("Pinging...");
+    m.edit(`Pong! ${m.createdTimestamp - message.createdTimestamp}ms`);
+  }   
+}
