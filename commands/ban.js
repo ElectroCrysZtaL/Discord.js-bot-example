459b2be4df2a91725b441b5076eb6206f1f814a8
ElectroCrysZtaL <41235770+ElectroCrysZtaL@users.noreply.github.com>
2018-10-05 12:14:10 +0700

    created log message

--- commands/ban.js
+++ commands/ban.js
@@ -2,4 +2,5 @@ exports.run = (message, client, args) => {
  // Your code
  const member = message.mentions.members.first();
  member.ban('reason');
+ console.log(`sucsessfully banned ${member.id}`)
 }
