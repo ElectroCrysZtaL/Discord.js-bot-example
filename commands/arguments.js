04b8df006d213540469b97b2ea355fde1d67abe1
ElectroCrysZtaL <41235770+ElectroCrysZtaL@users.noreply.github.com>
2018-11-20 13:31:54 +0700

    Update arguments.js

--- commands/arguments.js
+++ commands/arguments.js
@@ -2,7 +2,7 @@ exports.run = (client, message, args) => {
 
 const embed = new Discord.RichEmbed();
 .setTitle('**Arguments**')
-.addField(args[0])
+.addField(args[0]) // 1st argument will be with a space
 .addField(args[1])
 .addField(args[2])
 
